239b739e968a813b8471fe1b22e2132e2b32cb28
ForwardGT <102685124+ForwardGT@users.noreply.github.com>
2023-06-16 22:01:30 +0300

    clean don't used files and dependencies

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import Versions from './components/Versions.vue'
 import Timer from './components/Timer.vue'
 import Form from './components/Form.vue'
 </script>
 
 <template>
   <Timer></Timer>
-  <div class="gg" style="flex-direction: row;" v-for="i in 5">
+  <div class="gg" v-for="i in 5">
     <ul>
       <li> Текст <Form /></li>
     </ul>
