fc232e53e908e2f5e995998c0ab7cedd70eda79c
ForwardGT <102685124+ForwardGT@users.noreply.github.com>
2023-06-16 20:51:49 +0300

    first commit

---
+++ src/renderer/src/components/Form.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <p class="form">
+    <p class="number">1</p>
+  </p>
+</template>
+
+<style scoped>
+.form {
+  border: 2px solid;
+  height: 6vh;
+  width: 6vh;
+}
+</style>
---
+++ src/renderer/src/components/Timer.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+const clock = ref({
+  seconds1: 0,
+  seconds2: 0,
+  minutes1: 0,
+  minutes2: 0,
+  hours1: 0,
+  hours2: 0
+})
+
+function constPlus() {
+  clock.value.seconds1++
+  if (clock.value.seconds1 > 9) {
+    clock.value.seconds2++
+    clock.value.seconds1 = 0
+  }
+  if (clock.value.seconds2 > 5) {
+    clock.value.minutes1++
+    clock.value.seconds2 = 0
+  }
+  if (clock.value.minutes1 > 9) {
+    clock.value.minutes2++
+    clock.value.minutes1 = 0
+  }
+  if (clock.value.minutes2 > 5) {
+    clock.value.hours1++
+    clock.value.minutes2 = 0
+  }
+  if (clock.value.hours1 > 9) {
+    clock.value.hours2++
+    clock.value.hours1 = 0
+  }
+}
+
+setInterval(constPlus, 1000)
+</script>
+
+<template>
+  <div>
+    Таймер - {{ clock.hours2 }}{{ clock.hours1 }}:{{ clock.minutes2 }}{{ clock.minutes1 }}:{{
+      clock.seconds2
+    }}{{ clock.seconds1 }}
+  </div>
+  <button @click="">test</button>
+</template>
+
+<style scoped></style>
